9af5fa6305dee852059fe17db4d8323a281dd77d
win7n95 <win795@mail.ru>
2024-04-24 23:40:17 +0500

    add second homework

---
+++ Lesson_3/address.py
@@ -0,0 +1,15 @@
+class Address:
+    index = "620000"
+    town = "Ekaterinburg"
+    street = "Lenina"
+    home = "55"
+    kvartira = "22"
+   
+    def __init__(self, index, town, street, home, kvartira):
+        self.i = index
+        self.t = town
+        self.s = street
+        self.h = home
+        self.kv = kvartira
+
+        
---
+++ Lesson_3/lesson_3_task_2.py
@@ -0,0 +1,17 @@
+from smartphone import Smartphone
+
+phone1 = Smartphone
+phone2 = Smartphone
+phone3 = Smartphone
+phone4 = Smartphone
+phone5 = Smartphone
+phone1 = ("Apple", "SE2020", "+79992355587")
+phone2 = ("Lenovo", "10", "+79125698453")
+phone3 = ("Honor", "Deluxe", "+79089089898")
+phone4 = ("Samsung", "Galaxy S4", "+79876543211")
+phone5 = ("Redmi", "Note", "+79505336569")
+
+catalog = [phone1, phone2, phone3, phone4, phone5]
+
+for phones in catalog:
+    print(phones)
