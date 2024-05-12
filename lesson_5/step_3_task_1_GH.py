04c182778b5a3815851a92888f29509eddba8d79
WiN7N95 <150787224+WiN7N95@users.noreply.github.com>
2024-05-12 07:36:55 +0500

    Update step_3_task_1_GH.py

--- lesson_5/step_3_task_1_GH.py
+++ lesson_5/step_3_task_1_GH.py
@@ -9,9 +9,9 @@ driver.maximize_window()
 driver.get('http://the-internet.herokuapp.com/add_remove_elements/')
 
 for i in range(5):
-    addElButton = driver.find_element(By.CSS_SELECTOR, '[onclick="addElement()"]').click()
+    addElButton = driver.find_element(By.XPATH, ("//button[text()='Add Element']")).click()
 
-numDelButton = driver.find_elements(By.CSS_SELECTOR, "[class='added-manually']")
+numDelButton = driver.find_elements(By.XPATH, ("//button[text()='Delete']"))
 print(len(numDelButton))
 
 sleep(10)
